c159ac2ecd172cd450ada6042a4e0aca6bd13924
loezsilva <luizinho545@gmail.com>
2023-04-29 14:46:32 -0300

    Ajustes

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,5 @@
 const express = require('express')
-var cors = require('cors');
+const cors = require('cors');
 const path = require('path')
 const exceptionHandler = require('express-exception-handler')
 exceptionHandler.handle()
@@ -12,13 +12,13 @@ app.use(express.json())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
 // Allow all
-app.use(cors());
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../api/views'))
 global.WhatsAppInstances = {}
 
 const routes = require('../api/routes/')
 if (protectRoutes) {
+    app.use(cors());
     app.use(tokenCheck)
 }
 app.use('/', routes)
